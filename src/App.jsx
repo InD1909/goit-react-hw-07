77df8e80176032955ebd8a3f1386e24c64665a4d
InD1909 <ilyado1909@gmail.com>
2024-08-12 17:27:47 +0300

    task done

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import { fetchContacts } from "./redux/contactOps";
+import {
+  selectContacts,
+  selectLoading,
+  selectError,
+} from "./redux/contactsSlice";
+import { changeFilter, selectNameFilter } from "./redux/filtersSlice";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  const filter = useSelector(selectNameFilter);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const handleAddContact = (newContact) => {
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleFilterChange = (value) => {
+    dispatch(changeFilter(value));
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <SearchBox filter={filter} onFilter={handleFilterChange} />
+      {loading && <p>Loading contacts...</p>}
+      {error && <p>Error: {error}</p>}
+      <ContactList onDelete={handleDeleteContact} />
+    </div>
+  );
+};
+
+export default App;
